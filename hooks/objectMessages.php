fab99bc4458a8b521492fc2b0e0c050affa88ebe
Andrew Rivett <veggiefrog@gmail.com>
2016-03-11 20:07:45 -0500

    Added ability to get list of messages for a user via hooks/objectMessages

--- hooks/objectMessages.php
+++ hooks/objectMessages.php
@@ -31,7 +31,11 @@ function ciniki_sms_hooks_objectMessages($ciniki, $business_id, $args) {
 	}
 	$intl_timezone = $rc['settings']['intl-default-timezone'];
 	ciniki_core_loadMethod($ciniki, 'ciniki', 'users', 'private', 'datetimeFormat');
+	ciniki_core_loadMethod($ciniki, 'ciniki', 'users', 'private', 'dateFormat');
+	ciniki_core_loadMethod($ciniki, 'ciniki', 'users', 'private', 'timeFormat');
 	$datetime_format = ciniki_users_datetimeFormat($ciniki, 'php');
+	$date_format = ciniki_users_dateFormat($ciniki, 'php');
+	$time_format = ciniki_users_timeFormat($ciniki, 'php');
 
 	//
 	// Check for messages
@@ -47,13 +51,13 @@ function ciniki_sms_hooks_objectMessages($ciniki, $business_id, $args) {
 			. "ciniki_sms_messages.status AS status_text, "
 			. "ciniki_sms_messages.date_sent, "
 			. "ciniki_sms_messages.customer_id, "
-			. "ciniki_sms_messages.cell_phone, "
+			. "ciniki_sms_messages.cell_number, "
 			. "ciniki_sms_messages.content "
 			. "FROM ciniki_sms_objrefs, ciniki_sms_messages "
 			. "WHERE ciniki_sms_objrefs.business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
 			. "AND ciniki_sms_objrefs.object = '" . ciniki_core_dbQuote($ciniki, $args['object']) . "' "
 			. "AND ciniki_sms_objrefs.object_id = '" . ciniki_core_dbQuote($ciniki, $args['object_id']) . "' "
-			. "AND ciniki_sms_objrefs.sms_id = ciniki_sms.id "
+			. "AND ciniki_sms_objrefs.sms_id = ciniki_sms_messages.id "
 			. "AND ciniki_sms_messages.business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
 			. "";
 		if( isset($args['customer_id']) && $args['customer_id'] > 0 ) {
@@ -61,12 +65,54 @@ function ciniki_sms_hooks_objectMessages($ciniki, $business_id, $args) {
 		}
 		$strsql .= "ORDER BY ciniki_sms_messages.date_sent DESC "
 			. "";
-		ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQueryTree');
+		ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQueryArrayTree');
 		$rc = ciniki_core_dbHashQueryArrayTree($ciniki, $strsql, 'ciniki.sms', array(
 			array('container'=>'messages', 'fname'=>'id',
-				'fields'=>array('id', 'status', 'status_text', 'date_sent', 'customer_name', 'cell_phone', 'content'),
-				'maps'=>array('status_text'=>$maps['sms']['status']),
+				'fields'=>array('id', 'status', 'status_text', 'date_sent', 'cell_number', 'content'),
+				'maps'=>array('status_text'=>$maps['message']['status']),
 				'utctotz'=>array('date_sent'=>array('timezone'=>$intl_timezone, 'format'=>$datetime_format)),
+				),
+			));
+		if( $rc['stat'] != 'ok' ) {
+			return $rc;
+		}
+		if( isset($rc['messages']) ) {
+			return array('stat'=>'ok', 'messages'=>$rc['messages']);
+		}
+	}
+
+	elseif( isset($args['object']) && $args['object'] != '' 
+		&& isset($args['customer_id']) && $args['customer_id'] != ''
+		) {
+		//
+		// Check if there is any sms for this object
+		//
+		$strsql = "SELECT ciniki_sms_messages.id, "
+			. "ciniki_sms_messages.status, "
+			. "ciniki_sms_messages.status AS status_text, "
+			. "ciniki_sms_messages.date_sent, "
+			. "ciniki_sms_messages.date_sent AS sms_date, "
+			. "ciniki_sms_messages.date_sent AS sms_time, "
+			. "ciniki_sms_messages.customer_id, "
+			. "ciniki_sms_messages.cell_number, "
+			. "ciniki_sms_messages.content "
+			. "FROM ciniki_sms_objrefs, ciniki_sms_messages "
+			. "WHERE ciniki_sms_objrefs.business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+			. "AND ciniki_sms_objrefs.object = '" . ciniki_core_dbQuote($ciniki, $args['object']) . "' "
+			. "AND ciniki_sms_objrefs.sms_id = ciniki_sms_messages.id "
+			. "AND ciniki_sms_messages.customer_id = '" . ciniki_core_dbQuote($ciniki, $args['customer_id']) . "' "
+			. "AND ciniki_sms_messages.business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+			. "";
+		$strsql .= "ORDER BY ciniki_sms_messages.date_sent DESC "
+			. "";
+		ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQueryArrayTree');
+		$rc = ciniki_core_dbHashQueryArrayTree($ciniki, $strsql, 'ciniki.sms', array(
+			array('container'=>'messages', 'fname'=>'id',
+				'fields'=>array('id', 'status', 'status_text', 'date_sent', 'sms_date', 'sms_time', 'cell_number', 'content'),
+				'maps'=>array('status_text'=>$maps['message']['status']),
+				'utctotz'=>array('date_sent'=>array('timezone'=>$intl_timezone, 'format'=>$datetime_format),
+				    'sms_date'=>array('timezone'=>$intl_timezone, 'format'=>$date_format),
+				    'sms_time'=>array('timezone'=>$intl_timezone, 'format'=>$time_format)),
 				),
 			));
 		if( $rc['stat'] != 'ok' ) {
