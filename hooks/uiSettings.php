804746e9965d0d3f43c5526a1a8438d268041404
Andrew Rivett <veggiefrog@gmail.com>
2016-09-25 16:08:34 -0400

    Updated uiSettings for settings_menu_items

---
+++ hooks/uiSettings.php
@@ -0,0 +1,59 @@
+<?php
+//
+// Description
+// -----------
+// This function will return a list of user interface settings for the module.
+//
+// Arguments
+// ---------
+// ciniki:
+// business_id:     The ID of the business to get sms for.
+//
+// Returns
+// -------
+//
+function ciniki_sms_hooks_uiSettings($ciniki, $business_id, $args) {
+
+    //
+    // Setup the default response
+    //
+    $rsp = array('stat'=>'ok', 'menu_items'=>array(), 'settings_menu_items'=>array());
+
+    //
+    // Get the settings
+    //
+    $rc = ciniki_core_dbDetailsQueryDash($ciniki, 'ciniki_sms_settings', 'business_id', $business_id, 'ciniki.sms', 'settings', '');
+    if( $rc['stat'] == 'ok' && isset($rc['settings']) ) {
+        $rsp['settings'] = $rc['settings'];
+    }
+
+    //
+    // Check permissions for what menu items should be available
+    //
+    if( isset($ciniki['business']['modules']['ciniki.sms'])
+        && (isset($args['permissions']['owners'])
+            || isset($args['permissions']['employees'])
+            || isset($args['permissions']['resellers'])
+            || ($ciniki['session']['user']['perms']&0x01) == 0x01
+            )
+        ) {
+        $menu_item = array(
+            'priority'=>2000,
+            'label'=>'SMS', 
+            'edit'=>array('app'=>'ciniki.sms.main'),
+            );
+        $rsp['menu_items'][] = $menu_item;
+    } 
+
+    if( isset($ciniki['business']['modules']['ciniki.sms'])
+        && (isset($args['permissions']['owners'])
+            || isset($args['permissions']['resellers'])
+            || ($ciniki['session']['user']['perms']&0x01) == 0x01
+            )
+        ) {
+        $rsp['settings_menu_items'][] = array('priority'=>2000, 'label'=>'SMS', 'edit'=>array('app'=>'ciniki.sms.settings'));
+    }
+
+    return $rsp;
+}
+?>
